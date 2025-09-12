fd213f5bd3f9c3de7531090d75030e38042bd78b
Your Name <vemasani.pavani@gmail.com>
2025-09-12 22:31:55 +0530

    external dns

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # eks-terraform
+
+
+helm install external-dns bitnami/external-dns   --namespace default   --set provider=aws   --set aws.region=us-east-1   --set txtOwnerId=my-cluster   --set serviceAccount.name=dns-sa   --set serviceAccount.create=false
--- argo.sh
+++ argo.sh
@@ -13,6 +13,6 @@ fi
 if [ "$1" == "jobs" ]; then
     argocd login $(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1) --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
     for app in backend frontend ; do
-    argocd app create ${app} --repo https://github.com/pdevops78/expense-helm-argocd --path chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default.svc --insecure --skip-test-tls --grpc-web --values values/${app}.yml
+    argocd app create ${app} --repo https://github.com/pdevops78/eks-deploy-helm --path chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default.svc --insecure --skip-test-tls --grpc-web --values values/${app}.yml
     done
 fi
